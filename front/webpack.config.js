cb7177cb30f56966fdf499a4052dbc9e39ea7d3c
whassa <remi.graton@gmail.com>
2020-10-07 13:08:56 -0400

    Add functionning front end with react/babel/webpack

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,18 +1,30 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const port = process.env.PORT || 3000;
+const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
-  }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 3000
+  },
+  devtool: 'cheap-module-eval-source-map',
+  watch: true
 };
+
