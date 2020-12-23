5a7ae816fa01482ef1b1146f69aa91e8e3ba21b3
whassa <remi.graton@gmail.com>
2020-12-23 01:43:50 -0500

    Add some small details for menu

--- front/src/home.js
+++ front/src/home.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Form, Row, Col } from 'react-bootstrap';
+import { Form, Row, Col, Button } from 'react-bootstrap';
 class Home extends React.Component {
 
   render() {
     return (
       <div className="home">
         <Row className="justify-content-md-center align-middle home-row">
-          <Col sm={12} md={8} xl={6}>
+          <Col md={12} lg={6}>
               <Form className="home-form">
                 <h2 className="h2 home-title"> Welcome to QuickLink !</h2>
                 <Form.Group>
                   <Form.Control type="url" placeholder="Make a quick link !" />
                 </Form.Group>
+                <a href="#" class="stretched-link">Advance settings</a>
+                <Button className="home-button" variant="primary">Quick link!</Button>
               </Form>
           </Col>
         </Row>
